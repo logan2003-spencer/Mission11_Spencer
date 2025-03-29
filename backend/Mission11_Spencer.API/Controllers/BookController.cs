e607dac5bc7ced87ffa51a84feeae0c066b4eaeb
Logan Spencer <loganspencer203@gmail.com>
2025-03-28 23:02:12 -0600

    Your commit message here

--- backend/Mission11_Spencer.API/Controllers/BookController.cs
+++ backend/Mission11_Spencer.API/Controllers/BookController.cs
@@ -14,15 +14,23 @@ public class BookController : Controller
         _bookContext = temp;
     }
 
-    public IActionResult Get(int pageSize = 5, int pageNum = 1)
+    public IActionResult Get(int pageSize = 5, int pageNum = 1, [FromQuery] List<string>? bookTypes = null)
     {
+        var query = _bookContext.Books.AsQueryable();
+
+        if (bookTypes != null && bookTypes.Any())
+        {
+            query = query.Where(b => bookTypes.Contains(b.Category));
+        }
+        
+        var totalNumBooks = _bookContext.Books.Count();
+
         // Skip records based on page number, and take the records based on page size
-        var books = _bookContext.Books
+        var books = query
             .Skip((pageNum - 1) * pageSize)  // Skip previous pages
             .Take(pageSize)                 // Take the required number of books
             .ToList();
 
-        var totalNumBooks = _bookContext.Books.Count();
 
         var someObject = new
         {
@@ -32,6 +40,15 @@ public class BookController : Controller
         return Ok(someObject);
     }
 
+    [HttpGet("GetBookTypes")]
+    public IActionResult GetBookTypes()
+    {
+        var bookTypes = _bookContext.Books.Select(b => b.Category)
+            .Distinct()
+            .ToList();
+        
+        return Ok(bookTypes);
+    }
     
     
 }
