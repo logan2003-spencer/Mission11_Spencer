55052d66d70f84adb3ec9330f15e3fafdf93f30d
Logan Spencer <loganspencer203@gmail.com>
2025-04-02 15:41:11 -0600

    Committing changes before switching to Phase06

--- backend/Mission11_Spencer.API/obj/Debug/net8.0/Mission11_Spencer.API.AssemblyInfo.cs
+++ backend/Mission11_Spencer.API/obj/Debug/net8.0/Mission11_Spencer.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mission11_Spencer.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e539283c4515e5301255aa5a009dda4de3edfe9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0bcbe5aa47f4357c039582f68dd02f07b16713b7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mission11_Spencer.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mission11_Spencer.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
