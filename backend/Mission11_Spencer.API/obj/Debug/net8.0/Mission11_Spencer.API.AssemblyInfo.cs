d1f64533b50b02e07e9bf9ab5e533c50ea9e4f02
Logan Spencer <loganspencer203@gmail.com>
2025-04-02 09:47:45 -0600

    commit

--- backend/Mission11_Spencer.API/obj/Debug/net8.0/Mission11_Spencer.API.AssemblyInfo.cs
+++ backend/Mission11_Spencer.API/obj/Debug/net8.0/Mission11_Spencer.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mission11_Spencer.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c35d36212ee89d6b1272241cf6b56d8a94d50b34")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6a5a6da4a1aafd17b8e62d3c424081bccacc8c1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mission11_Spencer.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mission11_Spencer.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
