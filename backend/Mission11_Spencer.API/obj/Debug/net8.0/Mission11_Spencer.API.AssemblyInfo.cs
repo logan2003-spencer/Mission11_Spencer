81ebfb6a1eb5ab705f57f8fee82fe19e9676a484
Logan Spencer <loganspencer203@gmail.com>
2025-04-15 17:35:38 -0600

    Merged Phase06 into main, keeping all Phase06 changes

--- backend/Mission11_Spencer.API/obj/Debug/net8.0/Mission11_Spencer.API.AssemblyInfo.cs
+++ backend/Mission11_Spencer.API/obj/Debug/net8.0/Mission11_Spencer.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mission11_Spencer.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ddabf0a66611b34c2eeeb29bc7daacb4552fbe13")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99b0d55b936a51fd5c85c50aea7c189ec41272e9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mission11_Spencer.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mission11_Spencer.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
